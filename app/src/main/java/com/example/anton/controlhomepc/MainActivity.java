39c3f4daf5184acd43987de509fd8e72a77e62b9
Anton <nesterae1980@gmail.com>
2017-02-03 19:27:54 +0200

    rrrrr

---
+++ app/src/main/java/com/example/anton/controlhomepc/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.anton.controlhomepc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button sendUDP_btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sendUDP_btn = (Button) findViewById(R.id.sendUDPbutton);
+        sendUDP_btn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
